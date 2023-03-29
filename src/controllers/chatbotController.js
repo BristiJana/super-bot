3390a4af4940500fc43377d7d6acbfcb2bd1aa34
BristiJana <54547239+BristiJana@users.noreply.github.com>
2023-03-29 18:27:33 +0530

    Add files via upload

---
+++ src/controllers/chatbotController.js
@@ -0,0 +1,46 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
+
+let test = (req, res) => {
+  return res.send("Hello Again");
+};
+
+let getWebhook = (req, res) => {
+  let VERIFY_TOKEN = MY_VERIFY_TOKEN;
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+};
+let postWebhook = (req, res) => {
+  let body = req.body;
+
+  if (body.object === "page") {
+    body.entry.forEach(async function (entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
+};
+export default {
+  test,
+  getWebhook,
+  postWebhook,
+};
